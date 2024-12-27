b0285da20b63014ebe81a463bca3c51d01985305
Khyojun <khyojun1104@gmail.com>
2024-12-27 16:30:53 +0900

    [feat] #2 testContainer활용하여 진행할 수 있도록 테스트 코드 변경
    MONGODB 에서 nanoSecond 까지 검증이 되지 않아 epochSecond로 비교 진행

--- core/src/test/java/com/fc/NotificationRepositoryMemoryImplTest.java
+++ core/src/test/java/com/fc/NotificationRepositoryMemoryImplTest.java
@@ -7,10 +7,16 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+@SpringBootApplication
 class NotificationRepositoryMemoryImplTest {
 
-    private final NotificationRepositoryMemoryImpl sut = new NotificationRepositoryMemoryImpl();
+    @Autowired
+    private NotificationRepository sut;
 
     private final Instant now = Instant.now();
     private final Instant deletedAt = Instant.now().plus(90,ChronoUnit.DAYS);
@@ -18,11 +24,10 @@ class NotificationRepositoryMemoryImplTest {
     @Test
     void test_save() {
         // 알림 객체 생성
-
         // 저장
         // 조회했을 때 객체가 있냐?
 
-        Notification save = sut.save(
+        sut.save(
             new Notification("1", 2L, NotificationType.LIKE, now, deletedAt));
 
         Optional<Notification> notification = sut.findById("1");
@@ -42,8 +47,8 @@ class NotificationRepositoryMemoryImplTest {
 
         assertEquals(notification.id, "2");
         assertEquals(notification.userId, 2L);
-        assertEquals(notification.createdAt, now);
-        assertEquals(notification.deletedAt, deletedAt);
+        assertEquals(notification.createdAt.getEpochSecond(), now.getEpochSecond());
+        assertEquals(notification.deletedAt.getEpochSecond(), deletedAt.getEpochSecond());
     }
 
 
@@ -55,7 +60,7 @@ class NotificationRepositoryMemoryImplTest {
         sut.deleteById("3");
 
         Optional<Notification> notification = sut.findById("3");
-        
+
         assertTrue(notification.isEmpty());
     }
 
